5b97444ca9aa30be9b67e7c7a53e78023e49d027
Anton Bobko <antonbobko.by@gmail.com>
2022-11-06 12:00:40 +0300

    feat: add solution to the task 04-pocy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+const folder = path.join(__dirname, 'files');
+const folderCopy = path.join(__dirname, 'files-copy');
 
+async function folderCreate() {
+    await fs.promises.mkdir(folderCopy, {recursive: true}, (err) => {
+        if (err) {
+            return err;
+        }
+    });
+};
+
+async function performance() {
+    const itemFolder = await fs.promises.readdir(folder);
+    const itemFolderCopy = await fs.promises.readdir(folderCopy);
+    for (const file of itemFolderCopy) {
+        fs.promises.unlink(path.join(__dirname, 'files-copy', file));
+    };
+    if (itemFolder) {
+        for (const file of itemFolder) {
+            await fs.promises.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file));
+        }
+    };
+};
+
+folderCreate();
+performance();
